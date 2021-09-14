54c12b16a78bc6e989e61ba64233074a29c5c42e
Scotty Yackzan <syackzan@gmail.com>
2021-09-13 17:39:33 -0700

    Added path in require statements

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 // Require Files //
 const express = require('express');
+const path = require('path')
 const fs = require('fs');
 const notes = require('./db/db.json');
 
 //Initializing express
-const app = express;
+const app = express();
 const PORT = 3001;
 
 //Middleware for Parsing JSON and urlencoded form data
-app.request(express.json());
-app.request(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
-app.request(express.static('public'));
+app.use(express.static('public'));
 
 //Get Route For Universal HTML Page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 //Get Route For reading JSON File
-app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.get('api/notes', (req, res) => res.json(notes));
 
-app.post('api/notes', (req, res) => {
+// app.post('api/notes', (req, res) => {
+//     let data = req.body;
+//     const {title, text} = data;
+// });
 
-});
+app.listen(PORT, () =>
+    console.log(`APP is listening at http://localhost:${PORT}`)
+);
