1c0e02f37928cd2244e7575d7799a9f2cb4ac13f
Scotty Yackzan <syackzan@gmail.com>
2021-09-13 17:42:52 -0700

    Moved Universal Get request to bottom

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const fs = require('fs');
 const notes = require('./db/db.json');
 
+
 //Initializing express
 const app = express();
 const PORT = 3001;
@@ -14,22 +15,22 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static('public'));
 
-//Get Route For Universal HTML Page
-app.get('*', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 //Get Route For reading JSON File
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => res.json(notes));
 
 // app.post('api/notes', (req, res) => {
 //     let data = req.body;
 //     const {title, text} = data;
 // });
+
+//Get Route For Universal HTML Page
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
     console.log(`APP is listening at http://localhost:${PORT}`)
